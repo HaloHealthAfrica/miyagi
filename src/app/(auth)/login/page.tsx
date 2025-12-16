a2812e4dff563161d671698f353517d3db4a8cb4
SmartScaleProtypes <edwin.momanyi@gmail.com>
2025-12-16 00:14:31 -0500

    Force auth pages dynamic (fix POST /register 405)

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,6 +2,8 @@ import LoginForm from "./_form";
 import { Suspense } from "react";
 import { authenticateAction } from "./actions";
 
+export const dynamic = "force-dynamic";
+
 export default function LoginPage() {
 	return (
 		<Suspense>
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -2,6 +2,8 @@ import RegisterForm from "./_form";
 import { Suspense } from "react";
 import { registerAction } from "./actions";
 
+export const dynamic = "force-dynamic";
+
 export default function RegisterPage() {
 	return (
 		<Suspense>
